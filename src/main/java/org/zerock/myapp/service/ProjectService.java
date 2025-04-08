9ffa633f49d24e6268f4d34956524f7cdb63a38d
kongi <dbstjdal83@nate.com>
2025-04-09 00:48:21 +0900

    Repository Tests code 작성_01

--- src/main/java/org/zerock/myapp/service/ProjectService.java
+++ src/main/java/org/zerock/myapp/service/ProjectService.java
@@ -7,12 +7,12 @@ import org.zerock.myapp.entity.Project;
 
 public interface ProjectService {
 	
-	public abstract List<Project> getAllList();     			// 전체 조회
-	public abstract List<Project> getSearchList(ProjectDTO dto); // 전체 조회(검색)
+	public abstract List<Project> getAllList();     				// 전체 조회
+	public abstract List<Project> getSearchList(ProjectDTO dto); 	// 전체 조회(검색)
 	
-	public abstract Project create(ProjectDTO dto);    	// 생성 처리
-	public abstract Project getById(String id);    // 단일 조회
-	public abstract Boolean update(ProjectDTO dto);    	// 수정 처리
-	public abstract Boolean deleteById(String id);// 삭제 처리
+	public abstract Project create(ProjectDTO dto);    				// 생성 처리
+	public abstract Project getById(String id);    					// 단일 조회
+	public abstract Boolean update(ProjectDTO dto);    				// 수정 처리
+	public abstract Boolean deleteById(String id);					// 삭제 처리
 	
 }//end interface

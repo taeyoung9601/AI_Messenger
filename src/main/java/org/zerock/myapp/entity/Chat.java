9045bcb6f0eea8df628eea94b4d776d15ad6e95e
taeyoung9601 <rlaxoduddlqslek@naver.com>
2025-04-14 19:03:44 +0900

    chat entity update

--- src/main/java/org/zerock/myapp/entity/Chat.java
+++ src/main/java/org/zerock/myapp/entity/Chat.java
@@ -2,9 +2,8 @@ package org.zerock.myapp.entity;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.List;
-import java.util.Vector;
 
 import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.SourceType;
@@ -13,18 +12,18 @@ import org.zerock.myapp.util.BooleanToIntegerConverter;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.ToString;
 
 
 @Data
@@ -41,25 +40,24 @@ import lombok.ToString;
 public class Chat implements Serializable {
 	@Serial private static final long serialVersionUID = 1L;
 
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "ID", unique=true, nullable=false)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", unique=true, nullable=false, length= 500)
 	private Long id; // 채팅방 id
 
-	@Column(nullable=false)
+	@Column(nullable=false, length= 500)
 	private String name; // 채팅방명
 
 	@Convert(converter = BooleanToIntegerConverter.class)
-	@Column(nullable=false)
+	@Column(nullable=false, length= 500)
 	private Boolean enabled = true; // 활성화상태(1=유효,0=삭제)
 
 	
 	@CurrentTimestamp(event = EventType.INSERT, source = SourceType.DB)
-	@Column(name="CRT_DATE", nullable = false)
-	private Date crtDate; // 생성일
+	@Basic(optional = false, fetch = FetchType.LAZY)
+	private LocalDateTime crtDate; // 생성일
 
 	@CurrentTimestamp(event = EventType.UPDATE, source = SourceType.DB)
-	@Column(name="UDT_DATE")
-	private Date udtDate; // 수정일
+	private LocalDateTime udtDate; // 수정일
 
 	// join
 	@ManyToOne
@@ -69,12 +67,12 @@ public class Chat implements Serializable {
 //	@ToString.Exclude
 //	@OneToMany(mappedBy="Chat")
 //	private List<ChatEmployee> ChatEmployees = new Vector<>(); //  작성자 id 
-//
+
 //	@ToString.Exclude
 //	@OneToMany(mappedBy="Chat")
 //	private List<Message> Messages = new Vector<>(); // 메시지 id
 
-//	
+	
 //	public ChatEmployee addChatEmployee(ChatEmployee ChatEmployee) {
 //		getChatEmployees().add(ChatEmployee);
 //		ChatEmployee.setChat(this);
@@ -88,7 +86,7 @@ public class Chat implements Serializable {
 //
 //		return ChatEmployee;
 //	} // removeChatEmployee
-//
+
 //	public Message addMessage(Message Message) {
 //		getMessages().add(Message);
 //		Message.setChat(this);

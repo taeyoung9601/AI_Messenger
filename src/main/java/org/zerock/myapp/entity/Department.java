00142bd7bdf084b19c91d5baf686288090bc553e
Matthew <shin.joonchul@gmail.com>
2025-04-16 18:05:59 +0900

    department update

--- src/main/java/org/zerock/myapp/entity/Department.java
+++ src/main/java/org/zerock/myapp/entity/Department.java
@@ -7,9 +7,8 @@ import java.util.Date;
 import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.SourceType;
 import org.hibernate.generator.EventType;
+import org.zerock.myapp.domain.DepartmentDTO;
 import org.zerock.myapp.util.BooleanToIntegerConverter;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
@@ -23,11 +22,6 @@ import lombok.Data;
 
 
 @Data
-
-//JSON 으로 변환해서 보낼때, 제외 할 항목
-@JsonIgnoreProperties({
-	"udtDate"
-})
 
 // 부서 entity
 
@@ -63,48 +57,17 @@ public class Department implements Serializable {
 	
 	@Column(name="P_DEPT_ID")
 	private Long pDeptId; // 상위부서
-
 	
-	// join
-//	@ManyToOne
-//	@JoinColumn(name="P_DEPT_ID")
-//	private Department Department; // 상위부서
-//
-//	@ToString.Exclude
-//	@OneToMany(mappedBy="Department")
-//	private List<Department> Departments = new Vector<>(); // 부서
+	public DepartmentDTO toDto() {
+		DepartmentDTO dto = new DepartmentDTO();
+		dto.setId(this.id);
+		dto.setName(this.name);
+		dto.setDepth(this.depth);
+		dto.setEnabled(this.enabled);
+		dto.setPDeptId(this.pDeptId);
 
-//	@ToString.Exclude
-//	@OneToMany(mappedBy="Department")
-//	private List<Employee> Employees = new Vector<>(); // 사원
+	    return dto;
+	} // toDto
 
-
-//	public Department addDepartment(Department Department) {
-//		getDepartments().add(Department);
-//		Department.setDepartment(this);
-//
-//		return Department;
-//	} // addDepartment
-//
-//	public Department removeDepartment(Department Department) {
-//		getDepartments().remove(Department);
-//		Department.setDepartment(null);
-//
-//		return Department;
-//	} // removeDepartment
-
-//	public Employee addEmployee(Employee Employee) {
-//		getEmployees().add(Employee);
-//		Employee.setDepartment(this);
-//
-//		return Employee;
-//	} // addEmployee
-//
-//	public Employee removeEmployee(Employee Employee) {
-//		getEmployees().remove(Employee);
-//		Employee.setDepartment(null);
-//
-//		return Employee;
-//	} // removeEmployee
 
 } // end class

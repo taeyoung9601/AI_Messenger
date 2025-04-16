22cb646b4563edb44bd149c7df3a7ec282a8e3e1
Matthew <shin.joonchul@gmail.com>
2025-04-16 18:19:21 +0900

    dept component

--- src/main/java/org/zerock/myapp/controller/DepartmentController.java
+++ src/main/java/org/zerock/myapp/controller/DepartmentController.java
@@ -1,17 +1,12 @@
 package org.zerock.myapp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.zerock.myapp.entity.Department;
-import org.zerock.myapp.service.DepartmentService;
+import org.zerock.myapp.domain.DepartmentDTO;
+import org.zerock.myapp.service.DepartmentServiceImpl;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,51 +22,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/department")
 @RestController
 public class DepartmentController {
+    @Autowired private DepartmentServiceImpl service;
 	
-	@Autowired DepartmentService service;
-	
-	@GetMapping
-	List<Department> list() { // 리스트
-		log.debug("list() invoked.");
-		
-		List<Department> list = service.getAllList();
-		
-		return list;
-	} // list
-	
-	@PostMapping
-	String register() { // 등록 처리
-		log.debug("register() invoked.");
-		
-		return "register";
-	} // register
 	
 	@GetMapping(path = "/{id}")
-	String read( // 세부 조회
-			@PathVariable Long id
-			) {
+	DepartmentDTO read(@PathVariable Long id) { // 세부 조회
 		log.debug("read({}) invoked.",id);
 		
-		return "read";
+		// id를 통해 부서 조회
+		// 전부조회 = 1(회사)
+		DepartmentDTO dto =this.service.getByIdAndEnabled(id, true);
+		
+		return dto;
 	} // read
-	
-	@PutMapping(path = "/{id}")
-	String update( // 수정 처리
-			@PathVariable Long id
-			) { 
-		log.debug("update({}) invoked.",id);
-		
-		return "update";
-	} // update
-	
-	@DeleteMapping(path = "/{id}")
-	String delete( // 삭제 처리
-			@PathVariable Long id
-			) {
-		log.debug("delete({}) invoked.",id);
-		
-		return "delete";
-	} // delete
-	
 
 } // end class
